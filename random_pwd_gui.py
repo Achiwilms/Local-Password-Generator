414255628ae916f4ae0f08f0b409063eccd4c960
unknown <philip901203777@gmail.com>
2023-07-20 16:01:18 +0800

    [fix bug] error when no option selected, initial all ticked

--- random_pwd_gui.py
+++ random_pwd_gui.py
@@ -43,7 +43,7 @@ class pwd_app(ctk.CTk):
 		self.pwd_box.pack(padx=10, pady=10)
 		
         # password length label
-		self.pwd_len_label = ctk.CTkLabel(self, text="Password Length: "+str(self.pwd_len))
+		self.pwd_len_label = ctk.CTkLabel(self, text="Password Length: "+str(self.pwd_len), text_color="white")
 		self.pwd_len_label.pack()
 		
 		# password length silder
@@ -51,24 +51,28 @@ class pwd_app(ctk.CTk):
 		self.slider.pack(padx=10, pady=10)
 
 		# option: Uppercase
-		self.add_uppercase = ctk.BooleanVar(value=False)
-		self.option_upc = ctk.CTkCheckBox(self, text="Uppercase", variable=self.add_uppercase, onvalue=True, offvalue=False, command=self.check_box_state)
+		self.add_uppercase = ctk.BooleanVar(value=True)
+		self.option_upc = ctk.CTkCheckBox(self, text="Uppercase", variable=self.add_uppercase, onvalue=True, offvalue=False)
 		self.option_upc.pack(padx=10, pady=10)
+		self.option_upc.select()
 
 		# option: Lowercase
-		self.add_lowercase = ctk.BooleanVar(value=False)
-		self.option_loc = ctk.CTkCheckBox(self, text="Lowercase", variable=self.add_lowercase, onvalue=True, offvalue=False, command=self.check_box_state)
+		self.add_lowercase = ctk.BooleanVar(value=True)
+		self.option_loc = ctk.CTkCheckBox(self, text="Lowercase", variable=self.add_lowercase, onvalue=True, offvalue=False)
 		self.option_loc.pack(padx=10, pady=10)
+		self.option_loc.select()
 
 		# option: Number
-		self.add_number = ctk.BooleanVar(value=False)
-		self.option_num = ctk.CTkCheckBox(self, text="Number", variable=self.add_number, onvalue=True, offvalue=False, command=self.check_box_state)
+		self.add_number = ctk.BooleanVar(value=True)
+		self.option_num = ctk.CTkCheckBox(self, text="Number", variable=self.add_number, onvalue=True, offvalue=False)
 		self.option_num.pack(padx=10, pady=10)				
+		self.option_num.select()
 
 		# option: Symbol
-		self.add_symbol = ctk.BooleanVar(value=False)
-		self.option_sym = ctk.CTkCheckBox(self, text="Symbol", variable=self.add_symbol, onvalue=True, offvalue=False, command=self.check_box_state)
+		self.add_symbol = ctk.BooleanVar(value=True)
+		self.option_sym = ctk.CTkCheckBox(self, text="Symbol", variable=self.add_symbol, onvalue=True, offvalue=False)
 		self.option_sym.pack(padx=10, pady=10)
+		self.option_sym.select()
 
 		# generate buttom
 		self.gen_buttom = ctk.CTkButton(self, text="Generate", command=self.pwd_generation)
@@ -79,11 +83,7 @@ class pwd_app(ctk.CTk):
 		self.pwd_len = int(value);
 
 		# update password length label
-		self.pwd_len_label.configure(text = "Password Length: "+str(self.pwd_len))
-		return
-	
-	def check_box_state(self):
-		# print(self.add_symbol.get())
+		self.pwd_len_label.configure(text = "Password Length: "+str(self.pwd_len), text_color="white")
 		return
 
 	def pwd_generation(self):
@@ -101,7 +101,8 @@ class pwd_app(ctk.CTk):
 		
 		# no option selected
 		if (len(selected_option)==0):
-			
+			# error message
+			self.pwd_len_label.configure(text = "[ERROR] Please select at least one option!", text_color="red")
 			return
 		
 		# password generation
