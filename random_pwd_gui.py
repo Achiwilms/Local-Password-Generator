10282d9b1be0012e0c9daa85459480dabdd70a66
unknown <philip901203777@gmail.com>
2023-07-20 16:36:18 +0800

    some improvement in GUI

--- random_pwd_gui.py
+++ random_pwd_gui.py
@@ -5,7 +5,7 @@ import string
 
 # Settings
 # modes
-ctk.set_appearance_mode("System")
+ctk.set_appearance_mode("Dark")
 
 # themes
 ctk.set_default_color_theme("blue")
@@ -35,42 +35,45 @@ class pwd_app(ctk.CTk):
 		self.pwd = "";
 		
         # title label
-		self.title_label = ctk.CTkLabel(self, text="Random Password Generator")
+		self.title_label = ctk.CTkLabel(self, text="Random Password Generator", font=("Segoe UI", 25))
 		self.title_label.pack(padx=10, pady=10)
         
         # password box
-		self.pwd_box = ctk.CTkTextbox(self, width=600, height=100)	
+		self.pwd_box = ctk.CTkTextbox(self, width=600, height=100, font=("Segoe UI", 22))	
 		self.pwd_box.pack(padx=10, pady=10)
 		
         # password length label
-		self.pwd_len_label = ctk.CTkLabel(self, text="Password Length: "+str(self.pwd_len), text_color="white")
+		self.pwd_label_font = ("Segoe UI", 16)
+		self.pwd_len_label = ctk.CTkLabel(self, text="Password Length: "+str(self.pwd_len), text_color="white", font=self.pwd_label_font)
 		self.pwd_len_label.pack()
 		
 		# password length silder
 		self.slider = ctk.CTkSlider(self, from_=0, to=40, command=self.slider_event)        
 		self.slider.pack(padx=10, pady=10)
 
+		# option font
+		self.option_font = ("Segoe UI", 15)
 		# option: Uppercase
 		self.add_uppercase = ctk.BooleanVar(value=True)
-		self.option_upc = ctk.CTkCheckBox(self, text="Uppercase", variable=self.add_uppercase, onvalue=True, offvalue=False)
+		self.option_upc = ctk.CTkCheckBox(self, text="Uppercase", variable=self.add_uppercase, onvalue=True, offvalue=False, font = self.option_font)
 		self.option_upc.pack(padx=10, pady=10)
 		self.option_upc.select()
 
 		# option: Lowercase
 		self.add_lowercase = ctk.BooleanVar(value=True)
-		self.option_loc = ctk.CTkCheckBox(self, text="Lowercase", variable=self.add_lowercase, onvalue=True, offvalue=False)
+		self.option_loc = ctk.CTkCheckBox(self, text="Lowercase", variable=self.add_lowercase, onvalue=True, offvalue=False, font = self.option_font)
 		self.option_loc.pack(padx=10, pady=10)
 		self.option_loc.select()
 
 		# option: Number
 		self.add_number = ctk.BooleanVar(value=True)
-		self.option_num = ctk.CTkCheckBox(self, text="Number", variable=self.add_number, onvalue=True, offvalue=False)
+		self.option_num = ctk.CTkCheckBox(self, text="Number", variable=self.add_number, onvalue=True, offvalue=False, font = self.option_font)
 		self.option_num.pack(padx=10, pady=10)				
 		self.option_num.select()
 
 		# option: Symbol
 		self.add_symbol = ctk.BooleanVar(value=True)
-		self.option_sym = ctk.CTkCheckBox(self, text="Symbol", variable=self.add_symbol, onvalue=True, offvalue=False)
+		self.option_sym = ctk.CTkCheckBox(self, text="Symbol", variable=self.add_symbol, onvalue=True, offvalue=False, font = self.option_font)
 		self.option_sym.pack(padx=10, pady=10)
 		self.option_sym.select()
 
@@ -83,7 +86,7 @@ class pwd_app(ctk.CTk):
 		self.pwd_len = int(value);
 
 		# update password length label
-		self.pwd_len_label.configure(text = "Password Length: "+str(self.pwd_len), text_color="white")
+		self.pwd_len_label.configure(text = "Password Length: "+str(self.pwd_len), text_color="white", font=self.pwd_label_font)
 		return
 
 	def pwd_generation(self):
@@ -102,7 +105,7 @@ class pwd_app(ctk.CTk):
 		# no option selected
 		if (len(selected_option)==0):
 			# error message
-			self.pwd_len_label.configure(text = "[ERROR] Please select at least one option!", text_color="red")
+			self.pwd_len_label.configure(text = "[ERROR] Please select at least one option!", text_color="red", font=self.pwd_label_font)
 			return
 		
 		# password generation
